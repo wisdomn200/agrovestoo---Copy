942c0bbe8a3e52d69c2a007b201472af6b701771
Ekomobong Wisdom <61995296+wisdomn200@users.noreply.github.com>
2022-02-26 13:25:35 +0100

    Initial commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  ImagePath: string;
+  
+  constructor() {
+      //image location
+      this.ImagePath = '/assets/images/rice.jpg'
+      this.ImagePath =  '/assets/images/images/bellpepper.jpg'
+    }
+   
+  ngOnInit(): void {
+  }
+
+}
